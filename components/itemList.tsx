3cf87ddc9d92f09732aafc1bf4f2faa932074386
mikuweb <mxxx.kwtmg@gmail.com>
2023-04-21 16:43:02 +0900

    implement Delete item: handleDelete

--- components/itemList.tsx
+++ components/itemList.tsx
@@ -18,7 +18,7 @@ const ItemList: FC = () => {
 
   const [item, setItem] = useState<string>("");
 
-  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+  const handleAddItem = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const newItemList = [
       ...itemList,
@@ -32,6 +32,14 @@ const ItemList: FC = () => {
     setItem("");
   };
 
+  const handleDelete = (id) => {
+    const newItemList = itemList.filter((item) => {
+      return id !== item.id;
+    });
+
+    setItemList(newItemList);
+  };
+
   return (
     <>
       <div className="py-10">
@@ -41,32 +49,31 @@ const ItemList: FC = () => {
               key={item.id}
               className="flex justify-center gap-5 items-center my-5"
             >
-              <div className="bg-blue-50 rounded-2xl drop-shadow-lg text-blue-950 p-3.5 w-72 lg:w-96 h-14 flex items-center curser-pointer">
+              <div className="bg-blue-50 rounded-2xl drop-shadow-lg text-blue-950 p-3.5 w-72 lg:w-96 h-14 flex items-center cursor-pointer">
                 <p className="font-bold">{item.value}</p>
               </div>
-              <p>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke-width="1.5"
-                  stroke="currentColor"
-                  className="w-5 h-5"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
-                  />
-                </svg>
-              </p>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                className="w-5 h-5 cursor-pointer"
+                onClick={() => handleDelete(item.id)}
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                />
+              </svg>
             </li>
           ))}
         </ul>
       </div>
 
       <div className="">
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={handleAddItem}>
           <input
             id="add-item"
             value={item}
--- pages/index.tsx
+++ pages/index.tsx
@@ -22,33 +22,33 @@ export default function Home() {
             <p className="text-lg font-bold lg:text-2xl">3/5 Tasks achieved</p>
           </div>
 
-          <div className="py-10 mx-10 border-b-2 border-blue-100">
-            <ul className="flex gap-6 overflow-x-scroll">
-              <li className="date-card">
+          <div className="py-10 mx-auto w-72 lg:w-3/4 border-b-2 border-blue-100 ">
+            <ul className="flex gap-6  overflow-x-scroll">
+              <li className="w-16 h-20 rounded-2xl shadow-md flex flex-col items-center justify-center">
                 <p className="date">17</p>
                 <p>Mon</p>
               </li>
-              <li className="date-card">
+              <li className="w-16 h-20 rounded-2xl shadow-md flex flex-col items-center justify-center">
                 <p>18</p>
                 <p>Tue</p>
               </li>
-              <li className="date-card">
+              <li className="w-16 h-20 rounded-2xl shadow-md flex flex-col items-center justify-center">
                 <p>19</p>
                 <p>Wed</p>
               </li>
-              <li className="date-card">
+              <li className="w-16 h-20 rounded-2xl shadow-md flex flex-col items-center justify-center">
                 <p>20</p>
                 <p>Thu</p>
               </li>
-              <li className="date-card">
+              <li className="w-16 h-20 rounded-2xl shadow-md flex flex-col items-center justify-center">
                 <p>21</p>
                 <p>Fri</p>
               </li>
-              <li className="date-card">
+              <li className="w-16 h-20 rounded-2xl shadow-md flex flex-col items-center justify-center">
                 <p>22</p>
                 <p>Sat</p>
               </li>
-              <li className="date-card">
+              <li className="w-16 h-20 rounded-2xl shadow-md flex flex-col items-center justify-center">
                 <p>23</p>
                 <p>Sun</p>
               </li>
