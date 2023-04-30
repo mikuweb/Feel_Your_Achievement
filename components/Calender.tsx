324a515e1df796ba8009c7e8c3d213d7bfb63f9d
mikuweb <mxxx.kwtmg@gmail.com>
2023-04-30 20:46:14 +0900

    implement navigation buttons to show next/last week

--- components/Calender.tsx
+++ components/Calender.tsx
@@ -1,5 +1,11 @@
 import React, { useState, FC } from "react";
-import { endOfWeek, isWithinInterval, startOfWeek, format } from "date-fns";
+import {
+  endOfWeek,
+  isWithinInterval,
+  startOfWeek,
+  format,
+  addWeeks,
+} from "date-fns";
 
 import calendarStyles from "react-day-picker/dist/style.module.css";
 import { DayPicker, Row, RowProps, ClassNames } from "react-day-picker";
@@ -11,19 +17,19 @@ interface CalenderProps {
 }
 
 const Calender: FC<CalenderProps> = ({ selectedDay, setSelectedDay }) => {
-  const [weekNumber, setWeekNumber] = useState<number>();
+  const [selectedWeekOffset, setSelectedWeekOffset] = useState<number>(0); // <<: -1, >>: +1
 
   const CurrentWeekRow = (props: RowProps) => {
     const isDateInCurrentWeek = (dateToCheck: Date) => {
       const today = new Date();
-      const start = startOfWeek(today);
-      const end = endOfWeek(today);
-      return isWithinInterval(dateToCheck, { start, end });
+      const dayOfInterest = addWeeks(today, selectedWeekOffset); // what you want: today + selectedWeekOffset * weeks
+
+      const start = startOfWeek(dayOfInterest);
+      const end = endOfWeek(dayOfInterest);
+
+      return isWithinInterval(dateToCheck, { start, end }); // returns true if dateToCheck is within these two dates, false otherwise
     };
 
-    //↓filter functionにかえる
-    // 1.depends on the state [isSelectedweek]
-    // 2.月が替わったらこれ使ってその月を表示Controlling the current month
     const isNotCurrentWeek = props.dates.every(
       (date) => !isDateInCurrentWeek(date)
     );
@@ -35,50 +41,81 @@ const Calender: FC<CalenderProps> = ({ selectedDay, setSelectedDay }) => {
     setSelectedDay(date);
   };
 
-  const handleBack = (e: any) => {
-    console.log(e);
+  const handleBack = (e: React.MouseEvent<HTMLButtonElement>) => {
+    setSelectedWeekOffset(selectedWeekOffset - 1);
   };
 
-  const handleNext = (e: any) => {
-    console.log(e);
+  const handleNext = (e: React.MouseEvent<HTMLButtonElement>) => {
+    setSelectedWeekOffset(selectedWeekOffset + 1);
   };
 
   const classNames: ClassNames = {
     ...calendarStyles,
     months: styles.months,
+    day: styles.day,
   };
+  console.log(selectedWeekOffset);
   return (
     <div className="flex flex-col items-center">
       {selectedDay && `Day ${format(selectedDay, "d")},`}
-      {weekNumber && ` Week ${weekNumber}`}
       <DayPicker
         mode="single"
         selected={selectedDay}
         showOutsideDays
+        month={addWeeks(new Date(), selectedWeekOffset)} //"month" controls the displayed month / today + selectedWeekOffset
         classNames={classNames}
         modifiersClassNames={{
           selected: styles.selected,
           //ctrl + space
         }}
         onSelect={onSelectChange}
-        showWeekNumber
-        onWeekNumberClick={setWeekNumber}
         components={{
           Row: CurrentWeekRow,
         }}
         disableNavigation={true}
       />
       <div className="flex">
-        <button onClick={handleBack}>＜</button>
-        <button onClick={handleNext}>＞</button>
+        <button
+          className="relative rounded-full h-14 w-14 flex items-center justify-center p-4 hover:bg-slate-500 hover:bg-opacity-10 cursor-pointer transition "
+          onClick={handleBack}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M18.75 19.5l-7.5-7.5 7.5-7.5m-6 15L5.25 12l7.5-7.5"
+            />
+          </svg>
+        </button>
+        <button
+          className="relative rounded-full h-14 w-14 flex items-center justify-center p-4 hover:bg-slate-500 hover:bg-opacity-10 cursor-pointer transition "
+          onClick={handleNext}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M11.25 4.5l7.5 7.5-7.5 7.5m-6-15l7.5 7.5-7.5 7.5"
+            />
+          </svg>
+        </button>
       </div>
     </div>
   );
 };
 
 export default Calender;
-
-//1. hide <>
-//2. creatte own <>
-//3. you need some state to track which week you want to see (number or date)
-//4. connect to own <> buttons
