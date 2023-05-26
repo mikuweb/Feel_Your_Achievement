49de213e9ba6b6b44262a94202655fe32fa450a5
mikuweb <mikuweb@users.noreply.github.com>
2023-05-26 02:37:02 +0000

    Apply formatting changes

--- pages/signIn/index.tsx
+++ pages/signIn/index.tsx
@@ -1,6 +1,6 @@
 import { signIn, getCsrfToken, getProviders } from 'next-auth/react';
 import { GetServerSideProps } from 'next';
-import {BsGithub} from "react-icons/bs"
+import { BsGithub } from 'react-icons/bs';
 
 function signin({ providers = {} }) {
   return (
@@ -34,7 +34,9 @@ function signin({ providers = {} }) {
       >
         {/* CONTENT CONTAINER */}
         <div className=' p-10 flex flex-col justify-center items-center'>
-          <h3 className='text-3xl pb-10 font-semibold text-slate-600'>Sign in</h3>
+          <h3 className='text-3xl pb-10 font-semibold text-slate-600'>
+            Sign in
+          </h3>
           {/* Body */}
           <div
             className='
@@ -59,7 +61,7 @@ function signin({ providers = {} }) {
                   className='flex gap-2 justify-center items-center py-2 border-2 rounded-lg w-4/6 font-semibold hover:opacity-30'
                   onClick={() => signIn(provider.id)}
                 >
-                  <BsGithub size={20}/>
+                  <BsGithub size={20} />
                   Sign in with {provider.name}
                 </button>
               ))}
