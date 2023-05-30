7caf7aa3c8bef8bf048b7352fa26bd4c606db89a
mikuweb <mxxx.kwtmg@gmail.com>
2023-05-30 11:13:37 +0900

    markup habit ideas

--- pages/progress/index.tsx
+++ pages/progress/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Doughnut } from 'react-chartjs-2';
+// import { Doughnut } from 'react-chartjs-2';
 
 const Progress = () => {
   return (
     <div className='overflow-hidden bg-gradient-to-br from-indigo-300 to-sky-300 h-screen flex flex-col justify-center items-center'>
       <div className='text-white text-3xl lg:text-5xl font-bold m-10 text-center'>
-      <Doughnut data={} />
+      {/* <Doughnut data={} /> */}
       </div>
       <p className='text-white text-lg lg:text-xl'>We are working on it!</p>
     </div>
